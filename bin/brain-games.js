583d16659640a3186aee877980ed9f6177e66173
Andrei Shpak <rikoshet437@gmail.com>
2025-02-20 15:29:42 +0300

    add userGreeting, added readlineSync import and fixes

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import userGreeting from '../src/cli.js';
+
+userGreeting();
---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const userGreeting = () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+    return userName;
+};
+export default userGreeting;
