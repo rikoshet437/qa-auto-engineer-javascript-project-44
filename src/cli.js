6b0f7b274bb0c26d2aba47e3ceb26610daf09903
Andrei Shpak <rikoshet437@gmail.com>
2025-08-05 19:33:03 +0300

    fix userGreeting

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,5 @@ const userGreeting = () => {
     console.log('Welcome to the Brain Games!');
     const userName = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${userName}!`);
-    return userName;
 };
 export default userGreeting;
