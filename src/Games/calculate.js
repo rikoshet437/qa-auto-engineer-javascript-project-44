d9eaf9be65156e0064d3b2cbc5f16ebbe54f39dd
Andrei Shpak <rikoshet437@gmail.com>
2025-08-10 20:02:10 +0300

    Update calculate.js

--- src/Games/calculate.js
+++ src/Games/calculate.js
@@ -11,3 +11,20 @@ const calculate = (numOne, numTwo, operator) => {
         default: throw new Error('Something went wrong :(')
     }
 }
+const operators = ['+', '-', '*'];
+
+genGameRound = () => {
+    const numOne = genRandomNumber();
+    const numTwo = genRandomNumber();
+
+    const randomIndex = genRandomNumber(operators.length - 1);
+    const operator = operators[randomIndex];
+    const question = `${numOne} ${operator} ${numTwo}`;
+
+    const correctAnswer = calculate(numOne, numTwo, operator);
+    return [question, correctAnswer];
+}
+const startBrainCalc = () => {
+    runGeneralGameLogic(genGameRound, taskDescription);
+}
+export default startBrainCalc;
