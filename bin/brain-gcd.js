3dd5d2c6536285e37ac8151cb48265e8756c30b1
Andrei Shpak <rikoshet437@gmail.com>
2025-08-16 17:58:22 +0300

    Implemented brain-gcd and updates to support files

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import startBrainGcd from '../src/Games/Gcd.js';
+
+startBrainGcd();
--- src/Games/Gcd.js
+++ src/Games/Gcd.js
@@ -3,3 +3,27 @@ import runGeneralGameLogic from "../index.js";
 
 const taskDescription = 'Find the greatest common divisor of given numbers.';
 
+const gcd = (a, b) => {
+    let numOne = a;
+    let numTwo = b;
+    while (numTwo !== 0) {
+        const temp = numTwo;
+        numTwo = numOne % numTwo;
+        numOne = temp;
+    }
+    return numOne;
+};
+
+const genGameRound = () => {
+    const numOne = genRandomNumber(50, 1);
+    const numTwo = genRandomNumber(50, 1);
+    const question = `${numOne} ${numTwo}`;
+    const correctAnswer = gcd(numOne, numTwo).toString();
+    return [question, correctAnswer];
+};
+
+const startBrainGcd = () => {
+    runGeneralGameLogic(genGameRound, taskDescription);
+};
+
+export default startBrainGcd;
